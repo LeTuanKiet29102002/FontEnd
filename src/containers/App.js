d39c7b02a999eb11f430909e6f49f9d13dd0ceb5
LeTuanKiet29102002 <letuankiet02042k2@gmail.com>
2023-10-14 22:28:47 +0700

    slick-slider-specialty

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,8 @@ import Login from './Auth/Login';
 import Header from './Header/Header';
 import System from '../routes/System';
 
+import CustomScrollbars from '../components/CustomScrollbars';
+
 import { CustomToastCloseButton } from '../components/CustomToast';
 
 class App extends Component {
@@ -46,15 +48,16 @@ class App extends Component {
                 <Router history={history}>
                     <div className="main-container">
                         {this.props.isLoggedIn && <Header />}
-                        <span className="content-container">
+                        <div className="content-container">
+                        <CustomScrollbars style={{height:'100vh', width:'100%'}}>
                             <Switch>
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
                                 <Route path={path.HOMEPAGE} component={HomePage} />
-
                             </Switch>
-                        </span>
+                        </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
